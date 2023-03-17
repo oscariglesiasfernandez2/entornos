49dd1c91a285769bb6ac160f77ce6010a5608f79
oscari <oscari@DESKTOP-P7482NT>
2023-03-17 13:44:21 +0100

    segundo

--- eclipse-workspace/entornos/src/cuentas/CCuenta.java
+++ eclipse-workspace/entornos/src/cuentas/CCuenta.java
@@ -1,40 +1,107 @@
 package cuentas;
-
+/**
+ * probando cuentas 
+ * @author oscari
+ *
+ */
 public class CCuenta {
-	 private String nombre;
-	    private String cuenta;
-	    private double saldo;
-	    private double tipoInterés;
 
-	    public CCuenta()
-	    {
-	    }
+	private String nombre;
+	private String cuenta;
+	private double saldo;
+	private double tipoInterés;
 
-	    public CCuenta(String nom, String cue, double sal, double tipo)
-	    {
-	        nombre =nom;
-	        cuenta=cue;
-	        saldo=sal;
-	    }
+	/**
+	 * Permite asignar el tipo de interés
+	 * 
+	 * @param tipoInterés
+	 */
 
-	    public double estado()
-	    {
-	        return saldo;
-	    }
+	public void setTipoInterés(double tipoInterés) {
+		this.tipoInterés = tipoInterés;
+	}
 
-	    public void ingresar(double cantidad) throws Exception
-	    {
-	        if (cantidad<0)
-	            throw new Exception("No se puede ingresar una cantidad negativa");
-	        saldo = saldo + cantidad;
-	    }
+	/**
+	 * constructor vacio
+	 */
+	public CCuenta() {
+	}
 
-	    public void retirar(double cantidad) throws Exception
-	    {
-	        if (cantidad <= 0)
-	            throw new Exception ("No se puede retirar una cantidad negativa");
-	        if (estado()< cantidad)
-	            throw new Exception ("No se hay suficiente saldo");
-	        saldo = saldo - cantidad;
-	    }
+	/**
+	 * constructor completo
+	 * 
+	 * @param nom  que es nombre
+	 * @param cue  que es cuenta
+	 * @param sal  que es saldo
+	 * @param tipo que es el tipo
+	 */
+
+	public CCuenta(String nom, String cue, double sal, double tipo) {
+		nombre = nom;
+		cuenta = cue;
+		saldo = sal;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCuenta() {
+		return cuenta;
+	}
+
+	public void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public double getTipoInterés() {
+		return tipoInterés;
+	}
+
+	/**
+	 * 
+	 * @return devuelve el SALDO DE LA CUENTA
+	 */
+	public double estado() {
+		return saldo;
+	}
+
+	/**
+	 * PERMITE HACER INGRESOS
+	 * 
+	 * @param cantidad
+	 * @throws Exception
+	 */
+	public void ingresar(double cantidad) throws Exception {
+		if (cantidad < 0)
+			throw new Exception("No se puede ingresar una cantidad negativa");
+		saldo = saldo + cantidad;
+	}
+
+	/**
+	 * pERMITE HACER RETIRADAS
+	 * 
+	 * @param cantidad
+	 * @throws Exception
+	 */
+
+	public void retirar(double cantidad) throws Exception {
+		if (cantidad <= 0)
+			throw new Exception("No se puede retirar una cantidad negativa");
+		if (estado() < cantidad)
+			throw new Exception("No se hay suficiente saldo");
+		saldo = saldo - cantidad;
+	}
 }
